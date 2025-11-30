0b07ee235bd8b6ee8c7bb19272746ca115696865
Baisalov24 <baisalov.t98@gmail.com>
2025-11-30 13:07:30 -0600

    bug fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
     type = string
-    defauld = "us-east-2"
+    default = "us-east-2"
 }
 
 variable "instance_type" {
     type = string
-    defauld = "t2.micro"
+    default = "t2.micro"
 }
 
 variable "docker_image" {
